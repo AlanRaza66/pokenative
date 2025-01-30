94c1ce6b2869b21b1f6c0388c5d2f62050019ef4
Alan Raza <alanraza66@gmail.com>
2025-01-30 15:23:29 +0300

    feat: :sparkles: Base stat rendering.

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -3,6 +3,7 @@ import { RootView } from "@/components/RootView";
 import { Row } from "@/components/Row";
 import { ThemedText } from "@/components/ThemedText";
 import { PokemonSpec } from "@/components/pokemon/PokemonSpec";
+import { PokemonStat } from "@/components/pokemon/PokemonStat";
 import { PokemonType } from "@/components/pokemon/PokemonType";
 import { Colors } from "@/constants/Colors";
 import { formatSize, formatWeight, getPokemonArtwork } from "@/functions/pokemon";
@@ -41,13 +42,13 @@ export default function Pokemon() {
                 />
             </View>
             <Card style={[styles.card]}>
-                <Row style={[styles.centered]} gap={16}>
+                <Row gap={16}>
                     {types?.map((type) => {
                         return <PokemonType name={type.type.name} key={type.type.name} />
                     })}
                 </Row>
-                <ThemedText variant="subtitle1" style={[styles.centered, { color: colorType }]}>About</ThemedText>
-                <Row>
+                <ThemedText variant="subtitle1" style={[{ color: colorType }]}>About</ThemedText>
+                <Row style={{ alignSelf: "stretch" }}>
                     <PokemonSpec style={{ borderStyle: "solid", borderRightWidth: 1, borderColor: colors.grayLight }} title={formatWeight(pokemon?.weight)} description="Weight" image={require("@/assets/images/weight.png")} />
                     <PokemonSpec style={{ borderStyle: "solid", borderRightWidth: 1, borderColor: colors.grayLight }} title={formatSize(pokemon?.height)} description="Size" image={require("@/assets/images/height.png")} />
                     <PokemonSpec title={pokemon?.moves.slice(0, 2).map(m => m.move.name).join("\n")} description="Moves" />
@@ -55,7 +56,10 @@ export default function Pokemon() {
                 <ThemedText>{bio}</ThemedText>
 
                 {/* Stats */}
-                <ThemedText variant="subtitle1" style={[styles.centered, { color: colorType }]}>Base Stats</ThemedText>
+                <ThemedText variant="subtitle1" style={[{ color: colorType }]}>Base Stats</ThemedText>
+                <View style={{ alignSelf: "stretch" }}>
+                    {pokemon?.stats.map((s) => <PokemonStat name={s.stat.name} value={s.base_stat} color={colorType} key={s.stat.name} />)}
+                </View>
             </Card>
         </View>
     </RootView>
@@ -78,15 +82,15 @@ const styles = StyleSheet.create({
         top: -140,
     },
     body: {
+        alignSelf: "stretch",
         marginTop: 144,
         zIndex: 2
     },
     card: {
         paddingHorizontal: 20,
         paddingTop: 60,
+        paddingBottom: 20,
         gap: 16,
+        alignItems: "center"
     },
-    centered: {
-        alignSelf: "center"
-    }
 })
